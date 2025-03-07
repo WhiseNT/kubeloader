1fe8b84fe5ee68c74ca5a7bf38d92c3261883280
ZZZank <3410764033@qq.com>
2025-03-07 19:47:35 +0800

    rewrite ModContentPack to compute ScriptPack on demand

--- src/main/java/com/whisent/kubeloader/impl/mod/ModContentPack.java
+++ src/main/java/com/whisent/kubeloader/impl/mod/ModContentPack.java
@@ -1,13 +1,17 @@
 package com.whisent.kubeloader.impl.mod;
 
+import com.whisent.kubeloader.Kubeloader;
 import com.whisent.kubeloader.definition.ContentPack;
 import com.whisent.kubeloader.definition.PackLoadingContext;
-import dev.latvian.mods.kubejs.script.ScriptPack;
-import dev.latvian.mods.kubejs.script.ScriptType;
+import dev.latvian.mods.kubejs.script.*;
 import net.minecraftforge.forgespi.language.IModInfo;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.EnumMap;
 import java.util.Map;
+import java.util.jar.JarFile;
 
 /**
  * @author ZZZank
@@ -27,6 +31,36 @@ public class ModContentPack implements ContentPack {
 
     @Override
     public ScriptPack getPack(PackLoadingContext context) {
-        return packs.get(context.type());
+        return packs.computeIfAbsent(context.type(), k -> createPack(context));
+    }
+
+    private ScriptPack createPack(PackLoadingContext context) {
+        var pack = new ScriptPack(
+            context.manager(),
+            new ScriptPackInfo(mod.getModId(), "")
+        );
+
+        var prefix = Kubeloader.FOLDER_NAME + '/' + context.folderName() + '/';
+        try (var file = new JarFile(mod.getOwningFile().getFile().getFilePath().toFile())) {
+            var parent = file.getEntry(Kubeloader.FOLDER_NAME + '/' + context.folderName());
+            if (parent == null || !parent.isDirectory()) {
+                return null;
+            }
+            file.stream()
+                .filter(e -> !e.isDirectory())
+                .filter(e -> e.getName().endsWith(".js"))
+                .filter(e -> e.getName().startsWith(prefix))
+                .forEach(jarEntry -> {
+                    var fileInfo = new ScriptFileInfo(pack.info, jarEntry.getName());
+                    var scriptSource = (ScriptSource) info -> {
+                        var reader = new BufferedReader(new InputStreamReader(file.getInputStream(jarEntry)));
+                        return reader.lines().toList();
+                    };
+                    context.loadFile(pack, fileInfo, scriptSource);
+                });
+        } catch (IOException e) {
+            return null;
+        }
+        return pack;
     }
 }
