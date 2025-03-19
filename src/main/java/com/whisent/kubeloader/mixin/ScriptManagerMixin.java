9382af52e7c1431496f9a568c34eae49802c69c0
WhiseNT <107616400+WhiseNT@users.noreply.github.com>
2025-03-19 16:42:43 +0800

    根据内容包中的config内的priority来控制加载顺序，规则为： 0~10 高于原脚本加载顺序 -1~-10 低于原脚本加载顺序 数字越高加载顺序越早

--- src/main/java/com/whisent/kubeloader/mixin/ScriptManagerMixin.java
+++ src/main/java/com/whisent/kubeloader/mixin/ScriptManagerMixin.java
@@ -5,10 +5,8 @@ import com.whisent.kubeloader.definition.ContentPack;
 import com.whisent.kubeloader.definition.PackLoadingContext;
 import com.whisent.kubeloader.impl.ContentPackProviders;
 import dev.latvian.mods.kubejs.script.*;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
+import it.unimi.dsi.fastutil.Hash;
+import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -19,6 +17,7 @@ import java.util.*;
 @Mixin(ScriptManager.class)
 public abstract class ScriptManagerMixin {
 
+    @Mutable
     @Shadow
     @Final
     public Map<String, ScriptPack> packs;
@@ -26,39 +25,62 @@ public abstract class ScriptManagerMixin {
     @Inject(method = "reload", at = @At(value = "INVOKE", target = "Ldev/latvian/mods/kubejs/script/ScriptManager;load()V"), remap = false)
     private void injectPacks(CallbackInfo ci) {
         var context = new PackLoadingContext(thiz());
-        List<ContentPack>[] buckets = new List[21];
-        for (int i = 0; i < buckets.length; i++) {
-            buckets[i] = new ArrayList<>();
-        }
+
+        var newPack = new LinkedHashMap<String,ScriptPack>();
+        var originPack = new HashMap<String,ScriptPack>();
+        List<ContentPack>[] inferBuckets = new List[10];
+        List<ContentPack>[] deferBuckets = new List[10];
+
+        this.initBuckets(inferBuckets);
+        this.initBuckets(deferBuckets);
+
         for (var contentPack : ContentPackProviders.getPacks()) {
             Kubeloader.LOGGER.debug("寻找到contentPack: {}", contentPack);
-
             Object priority = contentPack.getConfig().get("priority");
 
-
+            int priorityInt = this.getPriorityInt(priority);
+            Kubeloader.LOGGER.debug("pack优先级"+priorityInt);
             var pack = contentPack.getPack(context);
             if (pack != null) {
-                this.packs.put(contentPack.getNamespace(context), contentPack.postProcessPack(context, pack));
-
-            }
-        }
-        Map<String, ScriptPack> newPack = new HashMap<>();
-        this.packs.forEach((key, value) -> {
-
-        });
-        /*
-        for (List<ContentPack> bucket : buckets) {
-            for (var contentPack : bucket) {
-                var pack = contentPack.getPack(context);
-                if (pack != null) {
-                    String namespace = contentPack.getNamespace(context);
-                    ScriptPack processedPack = contentPack.postProcessPack(context, pack);
-                    this.packs.put(namespace, processedPack);
+                if (priorityInt != 0) {
+                    if (priorityInt >= 0) {
+                        inferBuckets[9-priorityInt].add(contentPack);
+                    } else if (priorityInt < 0) {
+                        priorityInt = 1-priorityInt;
+                        deferBuckets[priorityInt].add(contentPack);
+                    }
+                } else {
+                    originPack.putAll(this.packs);
                 }
             }
         }
-         */
-
+        this.putPack(newPack,inferBuckets,context);
+        newPack.putAll(this.packs);
+        this.putPack(newPack,deferBuckets,context);
+        this.packs = newPack;
+    }
+    @Unique
+    private void initBuckets (List<ContentPack>[] Buckets) {
+        for (int i = 0; i < Buckets.length; i++) {
+            Buckets[i] = new ArrayList<>();
+        }
+    }
+    @Unique
+    private void putPack (LinkedHashMap<String,ScriptPack> newPack, List<ContentPack>[] Bucket , PackLoadingContext context) {
+        for (var bucket : Bucket) {
+            for (var contentPack : bucket) {
+                var pack = contentPack.getPack(context);
+                newPack.put(contentPack.getNamespace(context),contentPack.postProcessPack(context, pack));
+            }
+        }
+    }
+    @Unique
+    private int getPriorityInt (Object priority) {
+        int priorityInt = 0;
+        if (priority != null) {
+            priorityInt = Integer.parseInt(priority.toString());
+        }
+        return priorityInt;
     }
 
     @Unique
