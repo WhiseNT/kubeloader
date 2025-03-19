d231bdf5e91f0718b667a224804aec291cffe6e6
WhiseNT <107616400+WhiseNT@users.noreply.github.com>
2025-03-19 23:29:25 +0800

    完成了拓扑排序载入顺序 config内需要有以下key value来确定顺序 {     "isBefore": true, 	"target": "kubejs" }

--- src/main/java/com/whisent/kubeloader/mixin/ScriptManagerMixin.java
+++ src/main/java/com/whisent/kubeloader/mixin/ScriptManagerMixin.java
@@ -3,9 +3,10 @@ package com.whisent.kubeloader.mixin;
 import com.whisent.kubeloader.Kubeloader;
 import com.whisent.kubeloader.definition.ContentPack;
 import com.whisent.kubeloader.definition.PackLoadingContext;
+import com.whisent.kubeloader.files.topo.ContentPackSorter;
 import com.whisent.kubeloader.impl.ContentPackProviders;
 import dev.latvian.mods.kubejs.script.*;
-import it.unimi.dsi.fastutil.Hash;
+import dev.latvian.mods.kubejs.util.ConsoleJS;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -27,58 +28,39 @@ public abstract class ScriptManagerMixin {
         var context = new PackLoadingContext(thiz());
 
         var newPack = new LinkedHashMap<String,ScriptPack>();
-        List<ContentPack>[] inferBuckets = new List[10];
-        List<ContentPack>[] deferBuckets = new List[10];
+        var unsortedPacks = new LinkedHashMap<String,ContentPack>();
 
-        this.initBuckets(inferBuckets);
-        this.initBuckets(deferBuckets);
 
         for (var contentPack : ContentPackProviders.getPacks()) {
             Kubeloader.LOGGER.debug("寻找到contentPack: {}", contentPack);
-            Object priority = contentPack.getConfig().get("priority");
 
-            int priorityInt = this.getPriorityInt(priority);
-            Kubeloader.LOGGER.debug("pack优先级"+priorityInt);
             var pack = contentPack.getPack(context);
             if (pack != null) {
-                if (priorityInt != 0) {
-                    if (priorityInt >= 0) {
-                        inferBuckets[9-priorityInt].add(contentPack);
-                    } else if (priorityInt < 0) {
-                        priorityInt = 1-priorityInt;
-                        deferBuckets[priorityInt].add(contentPack);
-                    }
-                }
+                //统计所有的contentpacks
+                unsortedPacks.put(contentPack.getNamespace(),contentPack);
             }
         }
-        this.putPack(newPack,inferBuckets,context);
-        this.putPack(newPack,deferBuckets,context);
-        this.packs = newPack;
-    }
-    @Unique
-    private void initBuckets (List<ContentPack>[] Buckets) {
-        for (int i = 0; i < Buckets.length; i++) {
-            Buckets[i] = new ArrayList<>();
-        }
-    }
-    @Unique
-    private void putPack (LinkedHashMap<String,ScriptPack> newPack, List<ContentPack>[] Bucket , PackLoadingContext context) {
-        for (var bucket : Bucket) {
-            for (var contentPack : bucket) {
+        var Sorter = new ContentPackSorter(unsortedPacks);
+        Sorter.buildDependencies();
+        List<String> sortedOrder = Sorter.getSortedPacks();
+
+        ConsoleJS.STARTUP.log("排序后的pack为"+sortedOrder);
+        var originPack = new LinkedHashMap<String,ScriptPack>();
+        sortedOrder.forEach(namespace->{
+            if (!Objects.equals(namespace, "kubejs")) {
+                var contentPack = unsortedPacks.get(namespace);
+                Kubeloader.LOGGER.debug(namespace+"获取ContentPack为"+contentPack);
                 var pack = contentPack.getPack(context);
                 newPack.put(contentPack.getNamespace(context),contentPack.postProcessPack(context, pack));
+            } else {
+                originPack.putAll(this.packs);
+                newPack.putAll(originPack);
             }
-        }
+        });
+        this.packs = newPack;
+
     }
-    @Unique
-    private int getPriorityInt (Object priority) {
-        int priorityInt = 0;
-        if (priority != null) {
-            priorityInt = Integer.parseInt(priority.toString());
-            return priorityInt;
-        }
-        return priorityInt;
-    }
+
 
     @Unique
     private ScriptManager thiz() {
