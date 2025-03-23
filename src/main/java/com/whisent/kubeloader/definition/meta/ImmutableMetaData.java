734b793331fc31462e48f1cf782cb6ac1bd214ee
ZZZank <3410764033@qq.com>
2025-03-23 23:46:00 +0800

    interface instead of implementation for ArtifactVersion

--- src/main/java/com/whisent/kubeloader/definition/meta/ImmutableMetaData.java
+++ src/main/java/com/whisent/kubeloader/definition/meta/ImmutableMetaData.java
@@ -2,6 +2,7 @@ package com.whisent.kubeloader.definition.meta;
 
 import com.mojang.serialization.Codec;
 import com.whisent.kubeloader.definition.meta.dependency.PackDependency;
+import org.apache.maven.artifact.versioning.ArtifactVersion;
 import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
 
 import java.util.*;
@@ -13,9 +14,9 @@ record ImmutableMetaData(
     String id,
     Optional<String> name,
     Optional<String> description,
-    Optional<DefaultArtifactVersion> version,
+    Optional<ArtifactVersion> version,
     List<String> authors,
     List<PackDependency> dependencies
 ) implements PackMetaData {
-    static final Codec<DefaultArtifactVersion> VERSION_CODEC = Codec.STRING.xmap(DefaultArtifactVersion::new, DefaultArtifactVersion::toString);
+    static final Codec<ArtifactVersion> VERSION_CODEC = Codec.STRING.xmap(DefaultArtifactVersion::new, ArtifactVersion::toString);
 }
