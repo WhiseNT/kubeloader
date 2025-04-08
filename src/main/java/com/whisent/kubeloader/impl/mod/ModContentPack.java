3363000b29b9f56e024326a765bb0ca25ca0d9d0
WhiseNT <107616400+WhiseNT@users.noreply.github.com>
2025-04-09 00:15:31 +0800

    为ModContentPack添加了MetaData支持(未测试) - 等待重新整理代码（减少重复）

--- src/main/java/com/whisent/kubeloader/impl/mod/ModContentPack.java
+++ src/main/java/com/whisent/kubeloader/impl/mod/ModContentPack.java
@@ -1,8 +1,12 @@
 package com.whisent.kubeloader.impl.mod;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.mojang.serialization.JsonOps;
 import com.whisent.kubeloader.Kubeloader;
 import com.whisent.kubeloader.definition.ContentPack;
 import com.whisent.kubeloader.definition.PackLoadingContext;
+import com.whisent.kubeloader.definition.meta.PackMetaData;
 import dev.latvian.mods.kubejs.script.*;
 import net.minecraftforge.forgespi.language.IModInfo;
 import org.jetbrains.annotations.NotNull;
@@ -21,9 +25,11 @@ import java.util.jar.JarFile;
 public class ModContentPack implements ContentPack {
     private final IModInfo mod;
     private final Map<ScriptType, ScriptPack> packs = new EnumMap<>(ScriptType.class);
+    private final PackMetaData metaData;
 
     public ModContentPack(IModInfo mod) {
         this.mod = mod;
+        this.metaData = parseMetaData();
     }
 
     @Override
@@ -62,6 +68,60 @@ public class ModContentPack implements ContentPack {
             return null;
         }
         return pack;
+    }
+
+    @Override
+    public PackMetaData getMetaData() {
+        return metaData;
+    }
+
+    private PackMetaData parseMetaData() {
+        JsonObject jsonObject = searchMetaData();
+        if (jsonObject != null) {
+            var result = PackMetaData.CODEC.parse(
+                    JsonOps.INSTANCE,
+                    Kubeloader.GSON.fromJson(jsonObject, JsonObject.class)
+            );
+            if (result.result().isPresent()) {
+                return result.result().get();
+            } else {
+                return ContentPack.super.getMetaData();
+            }
+        } else {
+            return ContentPack.super.getMetaData();
+        }
+    }
+    private JsonObject searchMetaData() {
+        final JsonObject[] list = new JsonObject[1];
+        //搜索config文件
+        try (var file = new JarFile(mod.getOwningFile().getFile().getFilePath().toFile())) {
+            file.stream()
+                    .filter(e -> !e.isDirectory())
+                    .filter(e -> e.getName().endsWith(Kubeloader.META_DATA_FILE_NAME))
+                    .forEach(jarEntry -> {
+                        BufferedReader reader = null;
+                        try {
+                            reader = new BufferedReader(new InputStreamReader(file.getInputStream(jarEntry)));
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                        StringBuilder jsonContent = new StringBuilder();
+                        String line;
+                        while (true) {
+                            try {
+                                if ((line = reader.readLine()) == null) break;
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
+                            jsonContent.append(line);
+                        }
+                        JsonObject json = JsonParser.parseString(jsonContent.toString()).getAsJsonObject();
+                        list[0] = json;
+                    });
+            return list[0];
+        } catch (IOException e) {
+            return null;
+        }
     }
 
     @Override
